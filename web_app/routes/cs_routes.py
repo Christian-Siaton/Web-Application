936fc3e131a1b278f8e3d23d44be2e48936eb915
jsulam13 <101950152+jsulam13@users.noreply.github.com>
2022-12-09 15:03:05 -0500

    Web app (#3)
    * Still Local
    
    * table-first try
    
    * tables done
    
    Co-authored-by: Christian-Siaton <111945242+Christian-Siaton@users.noreply.github.com>

--- web_app/routes/cs_routes.py
+++ web_app/routes/cs_routes.py
@@ -27,7 +27,45 @@ def cs_dashboard():
     try:
 
         info = display_cashflow_statement(symbol=symbol)
-web-app
+
+        formatted_data = [
+            {"metric": "Fiscal Date Ending", "year_one": info[0]["Fiscal Date Ending"], "year_two": info[1]['Fiscal Date Ending'], "year_three": info[2]['Fiscal Date Ending'], "year_four": info[3]['Fiscal Date Ending'], "year_five": info[4]['Fiscal Date Ending']},
+            {"metric": "Reported Currency", "year_one": info[0]["Reported Currency"], "year_two": info[1]["Reported Currency"], "year_three": info[2]["Reported Currency"], "year_four": info[3]["Reported Currency"], "year_five": info[4]["Reported Currency"]},
+            {"metric": "OPERATING CASH FLOWS", "year_one": info[0]["OPERATING CASH FLOWS"], "year_two": info[1]["OPERATING CASH FLOWS"], "year_three": info[2]["OPERATING CASH FLOWS"], "year_four": info[3]["OPERATING CASH FLOWS"], "year_five": info[4]["OPERATING CASH FLOWS"]},
+            {"metric": "Payments for Operating Activities", "year_one": info[0]["Payments for Operating Activities"], "year_two": info[1]["Payments for Operating Activities"], "year_three": info[2]["Payments for Operating Activities"], "year_four": info[3]["Payments for Operating Activities"], "year_five": info[4]["Payments for Operating Activities"]},
+            {"metric": "Payments from Operating Activities", "year_one": info[0]["Payments from Operating Activities"], "year_two": info[1]["Payments from Operating Activities"], "year_three": info[2]["Payments from Operating Activities"], "year_four": info[3]["Payments from Operating Activities"], "year_five": info[4]["Payments from Operating Activities"]},
+            {"metric": "Change in Operating Liabilities", "year_one": info[0]["Change in Operating Liabilities"], "year_two": info[1]["Change in Operating Liabilities"], "year_three": info[2]["Change in Operating Liabilities"], "year_four": info[3]["Change in Operating Liabilities"], "year_five": info[4]["Change in Operating Liabilities"]},
+            {"metric": "Change in Operating Assets", "year_one": info[0]["Change in Operating Assets"], "year_two": info[1]["Change in Operating Assets"], "year_three": info[2]["Change in Operating Assets"], "year_four": info[3]["Change in Operating Assets"], "year_five": info[4]["Change in Operating Assets"]},
+            {"metric": "Depreciation Depletion and Amortization", "year_one": info[0]["Depreciation Depletion and Amortization"], "year_two": info[1]["Depreciation Depletion and Amortization"], "year_three": info[2]["Depreciation Depletion and Amortization"], "year_four": info[3]["Depreciation Depletion and Amortization"], "year_five": info[4]["Depreciation Depletion and Amortization"]},
+            {"metric": "Capital Expenditures", "year_one": info[0]["Capital Expenditures"], "year_two": info[1]["Capital Expenditures"], "year_three": info[2]["Capital Expenditures"], "year_four": info[3]["Capital Expenditures"], "year_five": info[4]["Capital Expenditures"]},
+            {"metric": "Profit Loss", "year_one": info[0]["Profit Loss"], "year_two": info[1]["Profit Loss"], "year_three": info[2]["Profit Loss"], "year_four": info[3]["Profit Loss"], "year_five": info[4]["Change in Receivables"]},
+            {"metric": "Change In Inventory", "year_one": info[0]["Change In Inventory"], "year_two": info[1]["Change In Inventory"], "year_three": info[2]["Change In Inventory"], "year_four": info[3]["Profit Loss"], "year_five": info[4]["Profit Loss"]},
+            {"metric": "CASH FLOW FROM INVESTMENT", "year_one": info[0]["CASH FLOW FROM INVESTMENT"], "year_two": info[1]["CASH FLOW FROM INVESTMENT"], "year_three": info[2]["CASH FLOW FROM INVESTMENT"], "year_four": info[3]["CASH FLOW FROM INVESTMENT"], "year_five": info[4]["CASH FLOW FROM INVESTMENT"]},
+            {"metric": "CASH FLOW FROM FINANCING", "year_one": info[0]["CASH FLOW FROM FINANCING"], "year_two": info[1]["CASH FLOW FROM FINANCING"], "year_three": info[2]["CASH FLOW FROM FINANCING"], "year_four": info[3]["CASH FLOW FROM FINANCING"], "year_five": info[4]["CASH FLOW FROM FINANCING"]},
+            {"metric": "Proceeds from Repayments of Short Term Debt", "year_one": info[0]["Proceeds from Repayments of Short Term Debt"], "year_two": info[1]["Proceeds from Repayments of Short Term Debt"], "year_three": info[2]["Proceeds from Repayments of Short Term Debt"], "year_four": info[3]["Proceeds from Repayments of Short Term Debt"], "year_five": info[4]["Proceeds from Repayments of Short Term Debt"]},
+            {"metric": "Payments for Repurchase of Common Stock", "year_one": info[0]["Payments for Repurchase of Common Stock"], "year_two": info[1]["Payments for Repurchase of Common Stock"], "year_three": info[2]["Payments for Repurchase of Common Stock"], "year_four": info[3]["Payments for Repurchase of Common Stock"], "year_five": info[4]["Payments for Repurchase of Common Stock"]},
+            {"metric": "Payments for Repurchase of Equity", "year_one": info[0]["Payments for Repurchase of Equity"], "year_two": info[1]["Payments for Repurchase of Equity"], "year_three": info[2]["Payments for Repurchase of Equity"], "year_four": info[3]["Payments for Repurchase of Equity"], "year_five": info[4]["Payments for Repurchase of Equity"]},
+            {"metric": "Payments for Repurchase of Preferred Stock", "year_one": info[0]["Payments for Repurchase of Preferred Stock"], "year_two": info[1]["Payments for Repurchase of Preferred Stock"], "year_three": info[2]["Payments for Repurchase of Preferred Stock"], "year_four": info[3]["Payments for Repurchase of Preferred Stock"], "year_five": info[4]["Payments for Repurchase of Preferred Stock"]},
+            {"metric": "Dividend Payout", "year_one": info[0]["Dividend Payout"], "year_two": info[1]["Dividend Payout"], "year_three": info[2]["Dividend Payout"], "year_four": info[3]["Dividend Payout"], "year_five": info[4]["Dividend Payout"]},
+            {"metric": "Dividend Payout Preferred Stock", "year_one": info[0]["Dividend Payout Preferred Stock"], "year_two": info[1]["Dividend Payout Preferred Stock"], "year_three": info[2]["Dividend Payout Preferred Stock"], "year_four": info[3]["Dividend Payout Preferred Stock"], "year_five": info[4]["Dividend Payout Preferred Stock"]},
+            {"metric": "Procceds from Issuance of Common Stock", "year_one": info[0]["Procceds from Issuance of Common Stock"], "year_two": info[1]["Procceds from Issuance of Common Stock"], "year_three": info[2]["Procceds from Issuance of Common Stock"], "year_four": info[3]["Procceds from Issuance of Common Stock"], "year_five": info[4]["Procceds from Issuance of Common Stock"]},
+            {"metric": "Proceeds from Issuance of Long Term Debt and Capital Securities", "year_one": info[0]["Proceeds from Issuance of Long Term Debt and Capital Securities"], "year_two": info[1]["Proceeds from Issuance of Long Term Debt and Capital Securities"], "year_three": info[2]["Proceeds from Issuance of Long Term Debt and Capital Securities"], "year_four": info[3]["Proceeds from Issuance of Long Term Debt and Capital Securities"], "year_five": info[4]["Proceeds from Issuance of Long Term Debt and Capital Securities"]},
+            {"metric": "Proceeds from Issuance of Preferred Stock", "year_one": info[0]["Proceeds from Issuance of Preferred Stock"], "year_two": info[1]["Proceeds from Issuance of Preferred Stock"], "year_three": info[2]["Proceeds from Issuance of Preferred Stock"], "year_four": info[3]["Proceeds from Issuance of Preferred Stock"], "year_five": info[4]["Proceeds from Issuance of Preferred Stock"]},
+            {"metric": "Proceeds from Repurchase of Equity", "year_one": info[0]["Proceeds from Repurchase of Equity"], "year_two": info[1]["Proceeds from Repurchase of Equity"], "year_three": info[2]["Proceeds from Repurchase of Equity"], "year_four": info[3]["Proceeds from Repurchase of Equity"], "year_five": info[4]["Proceeds from Repurchase of Equity"]},
+            {"metric": "Proceeds from Sale of Treasury Stock", "year_one": info[0]["Proceeds from Sale of Treasury Stock"], "year_two": info[1]["Proceeds from Sale of Treasury Stock"], "year_three": info[2]["Proceeds from Sale of Treasury Stock"], "year_four": info[3]["Proceeds from Sale of Treasury Stock"], "year_five": info[4]["Proceeds from Sale of Treasury Stock"]},
+            {"metric": "Change in Cash and Cash Equivlents", "year_one": info[0]["Change in Cash and Cash Equivlents"], "year_two": info[1]["Change in Cash and Cash Equivlents"], "year_three": info[2]["Change in Cash and Cash Equivlents"], "year_four": info[3]["Change in Cash and Cash Equivlents"], "year_five": info[4]["Change in Cash and Cash Equivlents"]},
+            {"metric": "Change in Exchange Rate", "year_one": info[0]["Change in Exchange Rate"], "year_two": info[1]["Change in Exchange Rate"], "year_three": info[2]["Change in Exchange Rate"], "year_four": info[3]["Change in Exchange Rate"], "year_five": info[4]["Change in Exchange Rate"]},
+            {"metric": "NET INCOME", "year_one": info[0]["NET INCOME"], "year_two": info[1]["NET INCOME"], "year_three": info[2]["NET INCOME"], "year_four": info[3]["NET INCOME"], "year_five": info[4]["NET INCOME"]}
+
+        ]
+
+
+        return render_template("cs_dashboard.html",
+            symbol=symbol,
+            info = info,
+            table_data = formatted_data
+
+        
         #breakpoint()
 
 
@@ -48,7 +86,6 @@ web-app
         return render_template("cs_dashboard.html",
             symbol=symbol,
             cs = info
-main
         )
     except Exception as err:
         print('OOPS', err)
--- web_app/routes/is_routes.py
+++ web_app/routes/is_routes.py
@@ -26,13 +26,54 @@ def is_dashboard():
 
     try:
 
-web-app
+
+        info= display_income_statement(symbol=symbol)
+
+
+
+
+        formatted_data = [
+            {"metric": "Fiscal Date Ending", "year_one": info[0]["Fiscal Date Ending"], "year_two": info[1]['Fiscal Date Ending'], "year_three": info[2]['Fiscal Date Ending'], "year_four": info[3]['Fiscal Date Ending'], "year_five": info[4]['Fiscal Date Ending']},
+            {"metric": "Reported Currency", "year_one": info[0]["Reported Currency"], "year_two": info[1]['Reported Currency'], "year_three": info[2]['Reported Currency'], "year_four": info[3]['Reported Currency'], "year_five": info[4]['Reported Currency']},
+            {"metric": "Total Revenue", "year_one": info[0]["Total Revenue"], "year_two": info[1]['Total Revenue'], "year_three": info[2]['Total Revenue'], "year_four": info[3]['Total Revenue'], "year_five": info[4]['Total Revenue']},
+            {"metric": "Cost of Revenue", "year_one": info[0]["Cost of Revenue"], "year_two": info[1]['Cost of Revenue'], "year_three": info[2]['Cost of Revenue'], "year_four": info[3]['Cost of Revenue'], "year_five": info[4]['Cost of Revenue']},
+            {"metric": "Cost of Goods and Services Sold", "year_one": info[0]["Cost of Goods and Services Sold"], "year_two": info[1]['Cost of Goods and Services Sold'], "year_three": info[2]['Cost of Goods and Services Sold'], "year_four": info[3]['Cost of Goods and Services Sold'], "year_five": info[4]['Cost of Goods and Services Sold']},
+            {"metric": "Operating Income", "year_one": info[0]["Operating Income"], "year_two": info[1]['Operating Income'], "year_three": info[2]['Operating Income'], "year_four": info[3]['Operating Income'], "year_five": info[4]['Operating Income']},
+            {"metric": "Selling, General, Administrative", "year_one": info[0]["Selling, General, Administrative"], "year_two": info[1]['Selling, General, Administrative'], "year_three": info[2]['Selling, General, Administrative'], "year_four": info[3]['Selling, General, Administrative'], "year_five": info[4]['Selling, General, Administrative']},
+            {"metric": "Research & Development", "year_one": info[0]["Research & Development"], "year_two": info[1]['Research & Development'], "year_three": info[2]['Research & Development'], "year_four": info[3]['Research & Development'], "year_five": info[4]['Research & Development']},
+            {"metric": "Net Investment Income", "year_one": info[0]["Net Investment Income"], "year_two": info[1]['Net Investment Income'], "year_three": info[2]['Net Investment Income'], "year_four": info[3]['Net Investment Income'], "year_five": info[4]['Net Investment Income']},
+            {"metric": "Interest Income", "year_one": info[0]["Interest Income"], "year_two": info[1]['Interest Income'], "year_three": info[2]['Interest Income'], "year_four": info[3]['Interest Income'], "year_five": info[4]['Interest Income']},
+            {"metric": "Interest Expense", "year_one": info[0]["Interest Expense"], "year_two": info[1]['Interest Expense'], "year_three": info[2]['Interest Expense'], "year_four": info[3]['Interest Expense'], "year_five": info[4]['Interest Expense']},
+            {"metric": "Non-interest Income", "year_one": info[0]["Non-interest Income"], "year_two": info[1]['Non-interest Income'], "year_three": info[2]['Non-interest Income'], "year_four": info[3]['Non-interest Income'], "year_five": info[4]['Non-interest Income']},
+            {"metric": "Other Non-Operating Income", "year_one": info[0]["Other Non-Operating Income"], "year_two": info[1]['Other Non-Operating Income'], "year_three": info[2]['Other Non-Operating Income'], "year_four": info[3]['Other Non-Operating Income'], "year_five": info[4]['Other Non-Operating Income']},
+            {"metric": "Depreciation", "year_one": info[0]["Depreciation"], "year_two": info[1]['Depreciation'], "year_three": info[2]['Depreciation'], "year_four": info[3]['Depreciation'], "year_five": info[4]['Depreciation']},
+            {"metric": "Depreciation and Amortization", "year_one": info[0]["Depreciation and Amortization"], "year_two": info[1]['Depreciation and Amortization'], "year_three": info[2]['Depreciation and Amortization'], "year_four": info[3]['Depreciation and Amortization'], "year_five": info[4]['Depreciation and Amortization']},
+            {"metric": "Income Before Tax", "year_one": info[0]["Income Before Tax"], "year_two": info[1]['Income Before Tax'], "year_three": info[2]['Income Before Tax'], "year_four": info[3]['Income Before Tax'], "year_five": info[4]['Income Before Tax']},
+            {"metric": "Income Tax Expense", "year_one": info[0]["Income Tax Expense"], "year_two": info[1]['Income Tax Expense'], "year_three": info[2]['Income Tax Expense'], "year_four": info[3]['Income Tax Expense'], "year_five": info[4]['Income Tax Expense']},
+            {"metric": "Interest and Debt Expense", "year_one": info[0]["Interest and Debt Expense"], "year_two": info[1]['Interest and Debt Expense'], "year_three": info[2]['Interest and Debt Expense'], "year_four": info[3]['Interest and Debt Expense'], "year_five": info[4]['Interest and Debt Expense']},
+            {"metric": "Net Income From Continuing Operations", "year_one": info[0]["Net Income From Continuing Operations"], "year_two": info[1]['Net Income From Continuing Operations'], "year_three": info[2]['Net Income From Continuing Operations'], "year_four": info[3]['Net Income From Continuing Operations'], "year_five": info[4]['Net Income From Continuing Operations']},
+            {"metric": "Comprehensive Income Net of Tax", "year_one": info[0]["Comprehensive Income Net of Tax"], "year_two": info[1]['Comprehensive Income Net of Tax'], "year_three": info[2]['Comprehensive Income Net of Tax'], "year_four": info[3]['Comprehensive Income Net of Tax'], "year_five": info[4]['Comprehensive Income Net of Tax']},
+            {"metric": "EBIT", "year_one": info[0]["EBIT"], "year_two": info[1]['EBIT'], "year_three": info[2]['EBIT'], "year_four": info[3]['EBIT'], "year_five": info[4]['EBIT']},
+            {"metric": "EBITDA", "year_one": info[0]["EBITDA"], "year_two": info[1]['EBITDA'], "year_three": info[2]['EBITDA'], "year_four": info[3]['EBITDA'], "year_five": info[4]['EBITDA']},
+            {"metric": "Net Income", "year_one": info[0]["Net Income"], "year_two": info[1]['Net Income'], "year_three": info[2]['Net Income'], "year_four": info[3]['Net Income'], "year_five": info[4]['Net Income']},
+
+        ]
+
+
+
+
+
+        clean_years = display_income_statement(symbol=symbol)
+        
+        return render_template("is_dashboard.html",
+            symbol=symbol,
+            table_data = formatted_data
+
         clean_years = display_income_statement(symbol=symbol)
 
         return render_template("is_dashboard.html",
             symbol=symbol,
             clean_years = clean_years
-
 
             #zero = ('Fiscal Date Ending: ' + clean_years[0]['fiscalDateEnding']),
             #one = ('Reported Currency: ' + clean_years[0]['reportedCurrency']),
@@ -63,7 +104,6 @@ web-app
         return render_template("is_dashboard.html",
             symbol=symbol,
             ics = info
-main
         )
     except Exception as err:
         print('OOPS', err)
